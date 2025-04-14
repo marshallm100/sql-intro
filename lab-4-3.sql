4b246bf88dd2f2b028e817ac1bd2b605550db8ad
Marshall Masterson <marshall.masterson@kellogg.northwestern.edu>
2025-04-14 18:07:24 +0000

    added answer for q4-3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,7 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+select teams.name, players.first_name, players.last_name, max(stats.home_runs) from teams inner join stats on stats.team_id = teams.id inner join players on players.id = stats.player_id where teams.year = 2019 order by max(stats.home_runs) limit 1;
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
